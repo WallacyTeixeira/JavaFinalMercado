b244133d53117b5675bddbc7324cf1cce63adbaf
Wallacy <wallacy.teixeira@outlook.com>
2023-12-07 13:45:45 -0300

    wallacy

--- MercadoJavaSA4/src/Model/Estoque.java
+++ MercadoJavaSA4/src/Model/Estoque.java
@@ -4,9 +4,46 @@ public class Estoque {
 
     private int idp;
     private String nome;
-    private int qunatia;
+    private int quantia;
     private boolean valor;
 
-    
+    public Estoque(int idp, String nome, int quantia, boolean valor) {
+        this.idp = idp;
+        this.nome = nome;
+        this.quantia = quantia;
+        this.valor = valor;
+    }
+
+    public int getIdp() {
+        return idp;
+    }
+
+    public void setIdp(int idp) {
+        this.idp = idp;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantia() {
+        return quantia;
+    }
+
+    public void setQuantia(int quantia) {
+        this.quantia = quantia;
+    }
+
+    public boolean isValor() {
+        return valor;
+    }
+
+    public void setValor(boolean valor) {
+        this.valor = valor;
+    }
 
 }
